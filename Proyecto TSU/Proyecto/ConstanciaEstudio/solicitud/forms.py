5ad56d32c50d2dd33fcef45264f1dff1419d502b
Victor2511 <125702846+Victor2511@users.noreply.github.com>
2024-08-31 22:40:36 -0400

    Update forms.py

--- Proyecto TSU/Proyecto/ConstanciaEstudio/solicitud/forms.py
+++ Proyecto TSU/Proyecto/ConstanciaEstudio/solicitud/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Constancia
 """Aqui se crea un formulario con los campos requeridos que esta vinculado al modelo de base de datos"""
 
+""" Actualizacion: Para validar de forma booleano el formulario y que sea tipo checkbox usaremos el campo primera_solicitud """
 
 class SolicitudForm(forms.ModelForm):
     class Meta:
